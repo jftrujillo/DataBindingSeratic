73d721160d3ff4cee9407b9fcb49b9a94685f5db
Jhon <jftrujillo@unicauca.edu.co>
2016-11-23 08:50:37 -0500

    picaso

--- app/src/main/java/com/example/jhon/databinding2/MainActivity.java
+++ app/src/main/java/com/example/jhon/databinding2/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.jhon.databinding2;
 
+import android.databinding.BindingAdapter;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.example.jhon.databinding2.databinding.ActivityMainBinding;
 import com.example.jhon.databinding2.models.Vehiculo;
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +21,13 @@ public class MainActivity extends AppCompatActivity {
         vehiculo.setMarca("Mazda");
         vehiculo.setModelo("2015");
         vehiculo.setTipo("Carro");
+        vehiculo.setImgUrl("https://co.mazdacdn.com/vpp/assets/thumbnails/cx5thumb.png");
         binding.setVehiculo(vehiculo);
+    }
+
+
+    @BindingAdapter({"bind:imageUrl"})
+    public static void loadImage(ImageView view, String url) {
+        Picasso.with(view.getContext()).load(url).into(view);
     }
 }
---
+++ app/src/main/java/com/example/jhon/databinding2/databinding/dataBindingMethods.java
@@ -0,0 +1,16 @@
+package com.example.jhon.databinding2.databinding;
+
+import android.databinding.BindingAdapter;
+import android.graphics.drawable.Drawable;
+import android.widget.ImageView;
+
+import com.example.jhon.databinding2.R;
+import com.squareup.picasso.Picasso;
+
+/**
+ * Created by jhon on 11/23/16.
+ */
+
+public class dataBindingMethods {
+
+}
