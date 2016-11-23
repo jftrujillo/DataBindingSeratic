729bc0b06f7d353f5d4163ec38d92815592a67e2
Jhon <jftrujillo@unicauca.edu.co>
2016-11-23 07:45:57 -0500

    first commit

---
+++ app/src/main/java/com/example/jhon/databinding2/models/Vehiculo.java
@@ -0,0 +1,49 @@
+package com.example.jhon.databinding2.models;
+
+/**
+ * Created by jhon on 11/23/16.
+ */
+
+public class Vehiculo {
+    String marca;
+    String modelo;
+    String tipo;
+    String imgUrl;
+
+    //region Getters and Setters
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    //endregion
+
+
+}
