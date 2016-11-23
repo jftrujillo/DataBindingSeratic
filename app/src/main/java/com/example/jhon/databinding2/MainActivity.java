93b6a24b5ea4da71f658705c15652891e2a18956
Jhon <jftrujillo@unicauca.edu.co>
2016-11-23 08:28:48 -0500

    Layout basico con dataBidnig

--- app/src/main/java/com/example/jhon/databinding2/MainActivity.java
+++ app/src/main/java/com/example/jhon/databinding2/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.jhon.databinding2;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.jhon.databinding2.databinding.ActivityMainBinding;
+import com.example.jhon.databinding2.models.Vehiculo;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.
+                setContentView(this,R.layout.activity_main);
+        Vehiculo vehiculo = new Vehiculo();
+        vehiculo.setMarca("Mazda");
+        vehiculo.setModelo("2015");
+        vehiculo.setTipo("Carro");
+        binding.setVehiculo(vehiculo);
     }
 }
